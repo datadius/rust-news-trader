6c47d0b693ef2336eee7522940e0603e6e07a35e
Data Dius <16731794+datadius@users.noreply.github.com>
2024-02-07 21:11:59 +0100

    added exchange info response to get precision

--- binance_news_rust/src/symbols_exchange_info.rs
+++ binance_news_rust/src/symbols_exchange_info.rs
@@ -5,17 +5,9 @@ use serde::Deserialize;
 pub struct ExchangeInfo {
     pub symbols: Vec<Symbol>,
 }
-
 #[allow(non_snake_case)]
 #[derive(Deserialize)]
 pub struct Symbol {
     pub symbol: String,
-    pub filters: Vec<Filter>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Deserialize)]
-pub struct Filter{
-    pub filterType: String,
-    pub stepSize: String,
+    pub quantityPrecision: i8,
 }
