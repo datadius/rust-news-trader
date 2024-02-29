247cf16e32da5fe05e0c43ef0b748483781a10b7
Data Dius <16731794+datadius@users.noreply.github.com>
2024-02-29 21:43:44 +0100

    updated bybit code to align scripts

--- news_rust/src/main.rs
+++ news_rust/src/main.rs
@@ -12,18 +12,18 @@ use price_information::PriceInformation;
 use symbol_information::SymbolInformation;
 use tree_response::TreeResponse;
 
-use env_logger;
+use fancy_regex::Regex;
 use fraction::Decimal;
-use futures::{future, StreamExt};
-use hex;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
 use hmac::Mac;
 use log::{error, info};
-use fancy_regex::Regex;
 use reqwest::{
     header::{HeaderMap, HeaderValue},
     Client,
 };
-use std::{collections::HashMap, env, error};
+use std::future::Future;
+use std::{collections::HashMap, env, error, pin::Pin};
 use tokio::time::{sleep, Duration};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, tungstenite::Result};
 
@@ -45,12 +45,20 @@ const EMPTY_TP_CASE: [TpInstance; 2] = [TpInstance { time: 0, pct: 0.0 }; 2];
 
 async fn market_buy_futures_position(
     client: Client,
-    symbol: &str,
+    symbol: String,
     qty: f32,
     qty_step: f32,
     tp_instance_arr: &[TpInstance; 2],
     recv_window: &str,
 ) -> Result<(), Box<dyn error::Error>> {
+    let price: f32 = get_price(client.clone(), &symbol).await?;
+    let leverage: f32 = get_leverage(client.clone(), &symbol, recv_window).await?;
+
+    let qty = Decimal::from(qty);
+    let qty_step_dec = Decimal::from(qty_step);
+    let leverage = Decimal::from(leverage);
+    let price = Decimal::from(price);
+    let qty = (qty * leverage / price / qty_step_dec).floor() * qty_step_dec;
     let url = "https://api-testnet.bybit.com/v5/order/create";
 
     let payload = format!(
@@ -67,10 +75,17 @@ async fn market_buy_futures_position(
     {
         let body = res.text().await?;
 
+        info!("Buy Futures Status {} = {}", &symbol, &body);
+
+        let qty: f32 = qty
+            .to_string()
+            .parse()
+            .expect("Failed to parse base coin qty");
+
         if tp_instance_arr[0].time != 0 {
             market_sell_position(
                 client,
-                symbol,
+                &symbol,
                 qty,
                 qty_step,
                 "linear",
@@ -79,17 +94,17 @@ async fn market_buy_futures_position(
             )
             .await?;
         } else {
-            error!("Failed to sell {}", body);
+            error!("Failed to sell {} {}", symbol, body);
         }
     } else {
-        error!("Error in sending the futures order");
+        error!("Error in sending the futures order {}", symbol);
     }
     Ok(())
 }
 
 async fn market_buy_spot_position(
     client: Client,
-    symbol: &str,
+    symbol: String,
     unit_qty: f32,
     qty_step: f32,
     tp_instance_arr: &[TpInstance; 2],
@@ -111,17 +126,19 @@ async fn market_buy_spot_position(
     {
         let body = res.text().await?;
 
+        info!("Buy Spot Status {} = {}", &symbol, &body);
+
         let order_response: Result<OrderResponse, _> = serde_json::from_str(&body);
 
         if let Ok(order) = order_response {
             if tp_instance_arr[0].time != 0 {
                 let qty = get_order_qty(client.clone(), &order.result.orderId, recv_window).await?;
-                let price = get_price(client.clone(), symbol).await?;
+                let price = get_price(client.clone(), &symbol).await?;
 
                 let tp_qty = ((qty / price) / qty_step).floor() * qty_step;
                 market_sell_position(
                     client,
-                    symbol,
+                    &symbol,
                     tp_qty,
                     qty_step,
                     "spot",
@@ -130,10 +147,10 @@ async fn market_buy_spot_position(
                 )
                 .await?;
             } else {
-                error!("Failed to sell {}", body);
+                error!("Failed to sell {} {}", symbol, body);
             }
         } else {
-            error!("Failed to buy {}", body);
+            error!("Failed to buy {} {}", symbol, body);
         }
     } else {
         error!("Error in sending the spot order");
@@ -154,7 +171,7 @@ async fn market_sell_position(
     let url = "https://api-testnet.bybit.com/v5/order/create";
 
     for tp in tp_instance_arr {
-        let seconds: u64 = tp.time as u64;
+        let seconds: u64 = tp.time;
         sleep(Duration::from_secs(seconds)).await;
         let tp_pct = Decimal::from(tp.pct);
         let qty_step_dec = Decimal::from(qty_step);
@@ -194,17 +211,18 @@ async fn get_leverage(
         .await?;
     let body = res.text().await?;
 
-    info!("Leverage = {}", body);
-
     let leverage_json: PositionList = serde_json::from_str(&body).unwrap_or(PositionList {
         result: position_list::Result {
-            list: vec!(position_list::LeverageList {
+            list: vec![position_list::LeverageList {
                 leverage: "0.0".to_string(),
-            }),
-        }
+            }],
+        },
     });
 
-    let value: f32 = leverage_json.result.list[0].leverage.parse().expect("Issue parsing the leverage to f32");
+    let value: f32 = leverage_json.result.list[0]
+        .leverage
+        .parse()
+        .expect("Issue parsing the leverage to f32");
 
     Ok(value)
 }
@@ -217,15 +235,19 @@ async fn get_price(client: Client, symbol: &str) -> Result<f32, Box<dyn error::E
     let res = client.get(&url).send().await?;
     let body = res.text().await?;
 
-    let price_information: PriceInformation = serde_json::from_str(&body).unwrap_or(PriceInformation {
-        result: price_information::PriceInformationResult {
-            list: vec!(price_information::PriceInformationList {
-                lastPrice: "0.0".to_string(),
-            }),
-        }
-    });
+    let price_information: PriceInformation =
+        serde_json::from_str(&body).unwrap_or(PriceInformation {
+            result: price_information::PriceInformationResult {
+                list: vec![price_information::PriceInformationList {
+                    lastPrice: "0.0".to_string(),
+                }],
+            },
+        });
 
-    let value: f32 = price_information.result.list[0].lastPrice.parse().expect("Issue parsing the price");
+    let value: f32 = price_information.result.list[0]
+        .lastPrice
+        .parse()
+        .expect("Issue parsing the price");
 
     Ok(value)
 }
@@ -268,7 +290,10 @@ async fn get_order_qty(
 
     let order_json: OrderInformation = serde_json::from_str(&body)?;
 
-    let qty: f32 = order_json.result.list[0].qty.parse().expect("Issue parsing the qty");
+    let qty: f32 = order_json.result.list[0]
+        .qty
+        .parse()
+        .expect("Issue parsing the qty");
 
     Ok(qty)
 }
@@ -277,10 +302,7 @@ fn title_case(title: &str) -> Result<(&str, TpCases), Box<dyn error::Error>> {
     if title.contains("Binance Will List") {
         Ok((r#"\([\d]*([^()]+)\)"#, TpCases::BinanceListing))
     } else if title.contains("마켓 디지털 자산 추가") {
-        Ok((
-            r#"\([\d]*([^()]+[^(\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3)])\)"#,
-            TpCases::UpbitListing,
-        ))
+        Ok((r#"[\( ](\w*)[,\)]"#, TpCases::UpbitListing))
     } else if title.contains("Binance Futures Will Launch USDⓈ-M") {
         Ok((
             r#"(?<=USDⓈ-M )\d*(.*)(?= Perpetual)"#,
@@ -293,20 +315,22 @@ fn title_case(title: &str) -> Result<(&str, TpCases), Box<dyn error::Error>> {
     }
 }
 
-fn process_title(title: &str) -> Result<(&str, TpCases), Box<dyn error::Error>> {
+fn process_title(title: &str) -> Result<(Vec<&str>, TpCases), Box<dyn error::Error>> {
     let (re_string, tp_case) = title_case(title)?;
     if tp_case == TpCases::NoListing {
-        return Ok(("", tp_case));
+        return Ok((vec![""], tp_case));
     }
     let re = Regex::new(re_string)?;
-    let symbol = re
-        .captures(title)
-        .expect("Issue applying the regex on title")
-        .expect("Issue capturing the symbol")
-        .get(1)
-        .map_or("", |m| m.as_str());
+    let symbols = re
+        .captures_iter(title)
+        .flatten()
+        .map(|m| m.get(1).expect("There was no group found"))
+        .map(|m| m.as_str())
+        .collect();
 
-    Ok((symbol, tp_case))
+    info!("Symbol: {:?}", symbols);
+
+    Ok((symbols, tp_case))
 }
 
 fn construct_headers(payload: &str, recv_window: &str) -> HeaderMap {
@@ -320,24 +344,33 @@ fn construct_headers(payload: &str, recv_window: &str) -> HeaderMap {
 
     let signature = {
         type HmacSha256 = hmac::Hmac<sha2::Sha256>;
-        let mut mac = HmacSha256::new_from_slice(&api_secret.as_bytes())
+        let mut mac = HmacSha256::new_from_slice(api_secret.as_bytes())
             .expect("HMAC can take key of any size");
         mac.update(to_sign.as_bytes());
         hex::encode(mac.finalize().into_bytes())
     };
 
     let mut headers = HeaderMap::new();
-    headers.insert("X-BAPI-API-KEY", HeaderValue::from_str(&api_key).expect("Issue processing the api key"));
-    headers.insert("X-BAPI-SIGN", HeaderValue::from_str(&signature).expect("Issue processing the signature"));
+    headers.insert(
+        "X-BAPI-API-KEY",
+        HeaderValue::from_str(&api_key).expect("Issue processing the api key"),
+    );
+    headers.insert(
+        "X-BAPI-SIGN",
+        HeaderValue::from_str(&signature).expect("Issue processing the signature"),
+    );
     headers.insert(
         "X-BAPI-TIMESTAMP",
         HeaderValue::from_str(&current_timestamp).expect("Issue processing the timestamp"),
     );
     headers.insert(
         "X-BAPI-RECV-WINDOW",
-        HeaderValue::from_str(&recv_window).expect("Issue processing the recv window"),
+        HeaderValue::from_str(recv_window).expect("Issue processing the recv window"),
     );
-    headers.insert("Connection", HeaderValue::from_str("keep-alive").expect("Issue processing the keep alive"));
+    headers.insert(
+        "Connection",
+        HeaderValue::from_str("keep-alive").expect("Issue processing the keep alive"),
+    );
     headers.insert(
         "Content-Type",
         HeaderValue::from_str("application/json").expect("Issue processing application/json"),
@@ -384,12 +417,9 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     tp_map.insert(
         TpCases::UpbitListing,
         [
+            TpInstance { time: 6, pct: 0.75 },
             TpInstance {
-                time: 2 * 60,
-                pct: 0.75,
-            },
-            TpInstance {
-                time: 13 * 60,
+                time: 10,
                 pct: 0.25,
             },
         ],
@@ -407,19 +437,13 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     tp_map.insert(
         TpCases::BithumbListing,
         [
-            TpInstance {
-                time: 90,
-                pct: 1.0,
-            },
-            TpInstance {
-                time: 0,
-                pct: 0.0,
-            },
+            TpInstance { time: 90, pct: 1.0 },
+            TpInstance { time: 0, pct: 0.0 },
         ],
     );
     loop {
         //wss://news.treeofalpha.com/ws ws://35.73.200.147:5050
-        if let Ok((mut socket, _)) = connect_async("wss://news.treeofalpha.com/ws").await {
+        if let Ok((mut socket, _)) = connect_async("ws://localhost:8765").await {
             while let Some(msg) = socket.next().await {
                 let msg = msg.unwrap_or(Message::binary(Vec::new()));
 
@@ -428,7 +452,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
 
                     info!("Response = {}", response);
 
-                    let tree_response: TreeResponse = match serde_json::from_str(&response) {
+                    let tree_response: TreeResponse = match serde_json::from_str(response) {
                         Ok(tree_response) => tree_response,
                         Err(e) => {
                             info!("Failed to parse tree response: {}", response);
@@ -437,56 +461,44 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
                         }
                     };
 
-                    let (symbol, tp_case) = process_title(&tree_response.title)?;
+                    let (symbols, tp_case) = process_title(&tree_response.title)?;
 
                     if tp_case != TpCases::NoListing {
-                        info!("symbol = {}", symbol);
+                        let mut handles =
+                            FuturesUnordered::<Pin<Box<dyn Future<Output = _>>>>::new();
+                        for symbol in symbols.iter() {
+                            info!("symbol = {}", symbol);
 
-                        let tp_instance_arr = tp_map.get(&tp_case).unwrap_or(&EMPTY_TP_CASE);
+                            let trade_pair = format!("{}USDT", symbol);
 
-                        let trade_pair = format!("{}USDT", symbol);
+                            let tp_instance_arr = tp_map.get(&tp_case).unwrap_or(&EMPTY_TP_CASE);
 
-                        let qty_step: f32 =
-                            get_symbol_information(client.clone(), &trade_pair).await?;
+                            let qty_step =
+                                get_symbol_information(client.clone(), &trade_pair).await?;
 
-                        let leverage: f32 =
-                            get_leverage(client.clone(), &trade_pair, recv_window).await?;
+                            handles.push(Box::pin(market_buy_futures_position(
+                                client.clone(),
+                                trade_pair.clone(),
+                                size_future,
+                                qty_step,
+                                tp_instance_arr,
+                                recv_window,
+                            )));
 
-                        info!("leverage = {}", leverage);
-
-                        let price: f32 = get_price(client.clone(), &trade_pair).await?;
-
-                        info!("price = {}", price);
-
-                        let qty = (size_future * leverage / price / qty_step).floor() * qty_step;
-
-                        info!("qty = {}", qty);
-
-                        let (_s, _g) = future::join(
-                            async {
-                                market_buy_futures_position(
-                                    client.clone(),
-                                    &trade_pair,
-                                    qty,
-                                    qty_step,
-                                    tp_instance_arr,
-                                    recv_window,
-                                )
-                                .await
-                            },
-                            async {
-                                market_buy_spot_position(
-                                    client.clone(),
-                                    &trade_pair,
-                                    size_spot,
-                                    qty_step,
-                                    tp_instance_arr,
-                                    recv_window,
-                                )
-                                .await
-                            },
-                        )
-                        .await;
+                            handles.push(Box::pin(market_buy_spot_position(
+                                client.clone(),
+                                trade_pair.clone(),
+                                size_spot,
+                                qty_step,
+                                tp_instance_arr,
+                                recv_window,
+                            )));
+                        }
+                        while let Some(result) = handles.next().await {
+                            if let Err(e) = result {
+                                error!("Failed to process trade pair: {}", e);
+                            }
+                        }
                     } else {
                         info!("Not a listing {}", &tree_response.title)
                     }
